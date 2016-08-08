a2b815b2fc4fff969c6c0581bb25834619593515
Shane Breeze <s.breeze15@imperial.ac.uk>
2016-08-08 15:16:43 +0100

    Example inputDataset for GENSIM step

--- StepGENSIM/test/crab_ICL-POWHEG_DMS_NNPDF30_13TeV_Pseudoscalar_100_1-GEN-SIM.py
+++ StepGENSIM/test/crab_ICL-POWHEG_DMS_NNPDF30_13TeV_Pseudoscalar_100_1-GEN-SIM.py
@@ -9,7 +9,7 @@ config.General.transferLogs    = True
 config.JobType.pluginName = 'ANALYSIS'
 config.JobType.psetName   = 'ICL-POWHEG_DMS_NNPDF30_13TeV-GEN-SIM_Hadronizer.py'
 
-config.Data.inputDataset         = '' # insert dataset
+config.Data.inputDataset         = '/NNPDF30_13TeV_Pseudoscalar_100_1-LHE/sbreeze-ICL-POWHEG_DMS_NNPDF30_13TeV_Pseudoscalar_100_1-LHE-d1e6a7a52c9e635a587ef1faf77a1500/USER' # insert dataset
 config.Data.inputDBS             = 'phys03'
 config.Data.splitting            = 'EventAwareLumiBased'
 config.Data.unitsPerJob          = 400
